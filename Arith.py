b295e9a0e45e746191992c952153a4f122e1ef4f
Shibu Mani <shibusinbox@outlook.com>
2020-06-10 12:29:10 +0530

    New addition

---
+++ Arith.py
@@ -0,0 +1,8 @@
+print("Count the chicken")
+print("Hens",25 + 30 / 6)
+print("Roosters", 100 - 25 * 4 % 4)
+print("Egg count")
+print(2 + 3 * 4 + 9 / 2 % 9 + 9)
+print("Is it true: 4 - 9 > 7 - 99")
+print(4 - 9 > 7 - 9)
+print("is it true: 5 - 4 > 6 - 9:", 5 - 4 > 7 - 9)
